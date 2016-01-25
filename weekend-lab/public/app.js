ee61a944d268c9e58b0410cf57e1976d58518a16
tkhuynh <thequest2311@yahoo.com>
2016-01-24 20:34:22 -0800

    finish user auth

--- weekend-lab/public/app.js
+++ weekend-lab/public/app.js
@@ -27,16 +27,53 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     });
 }]);
 
-app.controller('SelfiesIndexCtrl', ['$scope', function ($scope) {
+app.controller('SelfiesIndexCtrl', ['$scope', '$location', function ($scope, $location) {
   $scope.testing = "index";
 }]);
 
-app.controller('SignupCtrl', ['$scope', function ($scope) {
-  $scope.testing = "signup";
+app.controller('SignupCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+  $scope.scenario = 'Sign up';
+  $rootScope.currentUser = Parse.User.current();
+  
+  $scope.signUp = function(form) {
+    var user = new Parse.User();
+    user.set("email", form.email);
+    user.set("username", form.username);
+    user.set("password", form.password);
+    
+    user.signUp(null, {
+      success: function(user) {  
+        $rootScope.currentUser = user;
+        $location.path('/');
+        $scope.$apply();
+      },
+      error: function(user, error) {
+        alert("Unable to sign up:  " + error.code + " " + error.message);
+      }
+    });    
+  };
+
+  $rootScope.logOut = function(form) {
+    Parse.User.logOut();
+    $rootScope.currentUser = null;
+    $location.path('/signup');
+  };
 }]);
 
-app.controller('LoginCtrl', ['$scope', function ($scope) {
+app.controller('LoginCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
   $scope.testing = "login";
+  $scope.logIn = function(form) {
+    Parse.User.logIn(form.username, form.password, {
+      success: function(user) {
+        $rootScope.currentUser = user;
+        $location.path('/');
+        $scope.$apply();
+      },
+      error: function(user, error) {
+        alert("Unable to log in: " + error.code + " " + error.message);
+      }
+    });
+  };
 }]);
 
 app.controller('SelfiesShowCtrl', ['$scope', function ($scope) {
