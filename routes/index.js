f99cdb0e35215abf56757b3a3067a0ad34d6a079
vframbach <vframbach@gmail.com>
2016-02-06 15:40:26 -0800

    switched links to saved images and added one more

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,31 @@ var router = express.Router();
 
 var photos = [
 	{
-		url: 'https://farm3.staticflickr.com/2814/9056083794_4ae18d5d57_b.jpg',
+		url: '/images/9056083794_4ae18d5d57_b.jpg',
 		attribution: 'https://www.flickr.com/photos/chanc/',
 		latitude: 37.786428,
 		longitude: -122.408064
 	},
 	{
-		url: 'https://farm7.staticflickr.com/6165/6196724899_6ed2b786e9_b.jpg',
+		url: '/images/252435851_97653a89c0_b.jpg',
+		attribution: 'https://www.flickr.com/photos/mbiddulph',
+		latitude: 37.758319,
+		longitude: -122.427135
+	},
+	{
+		url: '/images/6196724899_6ed2b786e9_b.jpg',
 		attribution: 'https://www.flickr.com/photos/darwinbell/',
 		latitude: 37.776948,
 		longitude: -122.434489
 	},
 	{
-		url: 'https://farm2.staticflickr.com/1010/1285368842_81d9342b0b_b.jpg',
+		url: '/images/1285368842_81d9342b0b_b.jpg',
 		attribution: 'https://www.flickr.com/photos/araswami/',
 		latitude: 37.794491,
 		longitude: -122.392982
 	},
 	{
-		url: 'https://farm3.staticflickr.com/2778/4083210234_0e4f013454_b.jpg',
+		url: '/images/4083210234_0e4f013454_b.jpg',
 		attribution: 'https://www.flickr.com/photos/dahlstroms/',
 		latitude: 37.799629,
 		longitude: -122.415816
